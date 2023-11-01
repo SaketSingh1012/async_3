ac864979eab0d4914aaa16cb4bbe252af296e528
Saket Singh <saketaryanite@gmail.com>
2023-11-01 16:26:53 +0530

    async_3

--- client.js
+++ client.js
@@ -1,8 +1,26 @@
 const fetch = require("node-fetch");
 
-async function main() {
-  const response = await fetch("http://localhost:3000/initialize");
-  const data = await response.json();
-  console.log(data);
+async function fetchFilesInDirectory(data) {
+  const files = [];
+  for (const directory of data.items) {
+    if (directory.isDir) {
+      const directoryData = await fetch(`http://localhost:3000/${directory.name}`);
+      const file = await directoryData.json();
+        files.push(file);
+    }
+  }
+  return files;
 }
+
+async function main() {
+  const rootDirectory = await fetch("http://localhost:3000/");
+  if (rootDirectory.ok) {
+    const data = await rootDirectory.json();
+    console.log(data);
+    const fileNames = await fetchFilesInDirectory(data);
+    console.log("Files in the root directory:");
+    console.log(fileNames);
+  }
+}
+
 main();
